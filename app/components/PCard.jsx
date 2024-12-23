45477fa98d3df72c77421a7241832bd961a40d56
Pascal Ordano <pascalordanodev@gmail.com>
2024-12-23 10:16:46 -0300

    update

--- app/components/PCard.jsx
+++ app/components/PCard.jsx
@@ -7,7 +7,7 @@ export default function PCard( {projectTitle, imageSource, imageAlt} ){
             <div className="transition-all rounded-lg w-full bg-slate-800 flex flex-col items-center justify-center mx-auto py-10 hover:cursor-pointer">
                 <h1 className="text-2xl">{projectTitle}</h1>  
                 <Image 
-                    className="rounded-lg border mt-5"
+                    className="w-full rounded-lg border mt-5"
                     src={imageSource}
                     alt={imageAlt}
                     width={350}
--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <div className="justify-items-start">
         <h1 className="text-5xl font-bold transition-all animate-pulse">Pascal Ordano's Garden</h1>
-        <p className="mt-4 text-lg">A place where I share what I create and things that interest me</p>
+        <p className="mt-4 text-lg">A place where I share what I create and things that interest me.</p>
         <p>Im a software engineering student at ITBA ready to make cool sh*t</p>
       </div>
     </>
