ef802def3ee98c4e6565a0e86bbe8e3fc7a9ec4f
Pascal Ordano <pascalordanodev@gmail.com>
2025-05-08 21:16:06 -0300

    Added games section

---
+++ app/projects/mini_games/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useState } from "react";
+
+export default function MiniGames() { 
+
+    const [start, setStart] = useState(false);
+
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold">Mini Games</h1>
+            <p>A collection of unbeatable retro games</p>
+            <button className="text-white font-bold text-2xl hover:text-yellow-100 transition px-4 py-2 rounded-md" onClick={() => setStart(true)}>PLAY</button>
+        </div>
+    )
+}
