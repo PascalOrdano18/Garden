1013e652951f5070335d1316d25bd0918adbf533
Pascal Ordano <pascalordanodev@gmail.com>
2025-11-23 17:14:24 -0300

    btc

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 'use client';
 
 import ProgressiveText from '@/app/components/ProgressiveText';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+    
+    const [btcValue, setBtcValue] = useState<string | null>(null);
+
+    useEffect(() => {
+        getBtcValue();
+    });
+    
+
+    const getBtcValue = async () => {
+        const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'); 
+        if(!res.ok) return ;
+        const data = await res.json();
+        setBtcValue(data.bitcoin.usd);
+    }
+
+
   return (
     <div className="fade-in">
       <div className="justify-items-center space-y-6">
@@ -14,6 +31,13 @@ export default function Home() {
           I&apos;m a software engineering student at Instituto Tecnologico de Buenos
           Aires (ITBA)
         </p>
+
+        {btcValue && (
+            <p className='text-center'>
+                BTC at ${btcValue} usd btw
+            </p>
+        )}
+        
       </div>
     </div>
   );
