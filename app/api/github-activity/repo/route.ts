171049670587a8166bbbb50383bd7266fbcc3c92
Pascal Ordano <pascalordanodev@gmail.com>
2026-01-09 14:28:21 -0300

    fix

--- app/api/github-activity/repo/route.ts
+++ app/api/github-activity/repo/route.ts
@@ -1,19 +1,19 @@
-import { NextResponse, NextRequest } from 'next/server';
-
-
-
-export async function GET(req: NextRequest, { params: { username: string } }){
-    const username = await params;
-    try {
-        const res = await fetch(`https://api.github.com/users/${username}/repos`);
-        if(!res.ok) throw new Error("Api error");
-        const data = await res.json();
-        return NextResponse.json(data, { status: res.status });
-    } catch {
-        return NextResponse.json({ error: "API error" }, { status: res.status });
-    }
-}
-
+// import { NextResponse, NextRequest } from 'next/server';
+//
+//
+//
+// export async function GET(req: NextRequest, { params: { username: string } }){
+//     const username = await params;
+//     try {
+//         const res = await fetch(`https://api.github.com/users/${username}/repos`);
+//         if(!res.ok) throw new Error("Api error");
+//         const data = await res.json();
+//         return NextResponse.json(data, { status: res.status });
+//     } catch {
+//         return NextResponse.json({ error: "API error" }, { status: res.status });
+//     }
+// }
+//
 
 
 
